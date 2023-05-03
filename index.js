dcf1da98866598ad0975c281321590b6796c8f65
alex <lionwild@gmail.com>
2023-05-03 19:44:21 +0600

    change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
 
         fs.readFile(file, (error, data) => {
             if (error) {
-                res.writeHead(statusCode, { 'conten-type': 'text/html' })
+                res.writeHead(404, { 'conten-type': 'text/html' })
                 res.write(errorFile)
                 res.end();
             }
@@ -36,63 +36,29 @@ const server = http.createServer((req, res) => {
     else if (req.url == '/contact') {
         readFile(200, './publick/contact.html');
     }
-    // else if(req.url.match('\.css$')){
-    //     const cssFile = path.join(__dirname,'publick',req.url);
-    //     const readStream = fs.createReadStream(cssFile,'utf-8');
-    //     res.writeHead(200,{'content-type':'text/css'})
-    //     readStream.pipe(res);
-    // }
-
-    
     else if (req.url.match('\.css$')) {
         const cssFile = path.join(__dirname, 'publick', req.url);
-        if (fs.existsSync(cssFile)) {
-            const readStream = fs.createReadStream(cssFile, 'utf-8');
-            readStream.on('error', (err) => {
-                console.log(`Error reading file: ${err}`);
-                const errorCssFile = path.join(__dirname, 'publick', 'publick_css', 'error.css');
-                const errorCssStream = fs.createReadStream(errorCssFile, 'utf-8');
-                res.writeHead(200, { 'Content-Type': 'text/css' });
-                errorCssStream.pipe(res);
-            });
-            res.writeHead(200, { 'content-type': 'text/css' });
-            readStream.pipe(res);
-        } else {
+        const readFile = fs.createReadStream(cssFile,"utf-8");
+        readFile.on('error', (err) => {
+            console.log(`Error reading file: ${err}`);
             const errorCssFile = path.join(__dirname, 'publick', 'publick_css', 'error.css');
-            const errorCssStream = fs.createReadStream(errorCssFile, 'utf-8');
-            res.writeHead(200, { 'Content-Type': 'text/css' });
+            const errorCssStream = fs.createReadStream(errorCssFile,'utf-8');
+            res.writeHead(404, { 'Content-Type': 'text/css' });
             errorCssStream.pipe(res);
-        }
-    }
-    
-    
-    else if (req.url.match('\.gif$')) {
-        const imgFile = path.join(__dirname, 'publick', req.url);
-        const readStream = fs.createReadStream(imgFile);
-        
-        readStream.on('error', (err) => {
-          console.log(`Error reading file: ${err}`);
-          const errorImgFile = path.join(__dirname, 'publick', 'public-img', 'dribbble_1.gif');
-          const errorImgStream = fs.createReadStream(errorImgFile);
-          res.writeHead(200, { 'Content-Type': 'image/gif' });
-          errorImgStream.pipe(res);
         });
-        
-        res.writeHead(200, { 'content-type': 'image/gif' });
-        readStream.pipe(res);
-      }
+        res.writeHead(200,{'content-type':'text/css'})
+        readFile.pipe(res);
+    }
 else if (req.url.match('\.gif$')) {
   const imgFile = path.join(__dirname, 'publick', req.url);
   const readStream = fs.createReadStream(imgFile);
-  
   readStream.on('error', (err) => {
     console.log(`Error reading file: ${err}`);
     const errorImgFile = path.join(__dirname, 'publick', 'public-img', 'dribbble_1.gif');
     const errorImgStream = fs.createReadStream(errorImgFile);
-    res.writeHead(200, { 'Content-Type': 'image/gif' });
+    res.writeHead(404, { 'Content-Type': 'image/gif' });
     errorImgStream.pipe(res);
   });
-  
   res.writeHead(200, { 'content-type': 'image/gif' });
   readStream.pipe(res);
 }
@@ -100,15 +66,13 @@ else if (req.url.match('\.gif$')) {
 else if (req.url.match('\.mp4$')) {
     const imgFile = path.join(__dirname, 'publick', req.url);
     const readStream = fs.createReadStream(imgFile);
-  
     readStream.on('error', (err) => {
       console.log(`Error reading file: ${err}`);
       const defaultVideoFile = path.join(__dirname, 'publick', 'public-img', 'Lama_Dribbble.mp4');
       const defaultVideoStream = fs.createReadStream(defaultVideoFile);
-      res.writeHead(200, { 'content-type': 'video/mp4' });
+      res.writeHead(404, { 'content-type': 'video/mp4' });
       defaultVideoStream.pipe(res);
     });
-  
     res.writeHead(200, { 'content-type': 'video/mp4' });
     readStream.pipe(res);
   }
